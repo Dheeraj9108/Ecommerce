57a6fa1d86f5eb4ec7f44afb7e26ca7e1857f098
Dheeraj <poojarydheeraj665@gmail.com>
2022-06-24 14:02:21 +0530

    test

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { signup, signin, requireSigin } = require("../controller/auth");
 
+const { validateRequest, isRequestValidated } = require("../validator/auth");
+
 const router = express.Router();
 
 
-router.post('/signup', signup);
+router.post('/signup',validateRequest,isRequestValidated,signup);
 router.post('/signin', signin);
 
 
